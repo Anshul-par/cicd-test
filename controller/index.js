c2036c0b71c6b8ee2e75317ec507cbb98bd886ae
Anshul-par <anshul.borde@parsolution.net>
2024-08-31 16:23:01 +0530

    third commit

--- controller/index.js
+++ controller/index.js
@@ -4,14 +4,6 @@ export const handleGithubHook = async (req, res) => {
 
     console.log("payload", payload);
 
-    // Check if the push is to the main branch
-    if (payload.ref === "refs/heads/main") {
-      console.log("Push to main branch detected.");
-
-      // Handle the logic for changes in the main branch
-      // e.g., deploy code, run tests, send notifications, etc.
-    }
-
     res.sendStatus(200);
   } catch (error) {
     console.log("Error in handleGithubHook", error);
